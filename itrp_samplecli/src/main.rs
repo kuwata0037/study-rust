c6027c02a6e13ff8ba176a3f658150f86c6d5941
kuwata0037 <kuwata0037@gmail.com>
2020-09-09 21:48:03 +0900

    clapのbuilderパータンを利用したコマンドライン引数の処理

--- itrp_samplecli/src/main.rs
+++ itrp_samplecli/src/main.rs
@@ -1,6 +1,31 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My RPN program")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Your name")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::with_name("formula_file")
+                .about("Formulas written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .about("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
