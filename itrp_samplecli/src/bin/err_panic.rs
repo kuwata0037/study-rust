31e0d7487c1388810271cff493e7e51eb647ca9d
kuwata0037 <kuwata0037@gmail.com>
2020-09-10 22:39:20 +0900

    独自のエラー型を定義

--- itrp_samplecli/src/bin/err_panic.rs
+++ itrp_samplecli/src/bin/err_panic.rs
@@ -1,12 +1,28 @@
-fn get_int_from_file() -> Result<i32, String> {
+use std::fmt;
+
+enum MyError {
+    Io(std::io::Error),
+    Num(std::num::ParseIntError),
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Io(cause) => write!(f, "I/O Error: {}", cause),
+            Self::Num(cause) => write!(f, "Parse Error: {}", cause),
+        }
+    }
+}
+
+fn get_int_from_file() -> Result<i32, MyError> {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
 
     num_str
         .trim()
         .parse::<i32>()
         .map(|n| n * 2)
-        .map_err(|e| e.to_string())
+        .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
