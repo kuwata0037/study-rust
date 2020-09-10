6dd90f4600640294c2052193c1f50995481a3906
kuwata0037 <kuwata0037@gmail.com>
2020-09-10 22:28:49 +0900

    Result でエラーを受け取る

--- itrp_samplecli/src/bin/err_panic.rs
+++ itrp_samplecli/src/bin/err_panic.rs
@@ -1,15 +1,17 @@
-fn get_int_from_file() -> i32 {
+fn get_int_from_file() -> Result<i32, String> {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
+    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
 
-    let ret = num_str
+    num_str
         .trim()
         .parse::<i32>()
-        .expect("failed to parse string to a number");
-
-    ret * 2
+        .map(|n| n * 2)
+        .map_err(|e| e.to_string())
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
 }
