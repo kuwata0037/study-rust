75ae0e091880828a0faffb55f028e31529e5caf8
kuwata0037 <kuwata0037@gmail.com>
2021-01-27 20:46:01 +0900

    ドキュメントを追加
    実践Rust第10章

--- bicycle-book/src/ch10.rs
+++ bicycle-book/src/ch10.rs
@@ -1,20 +1,38 @@
+//! wordcount はシンプルな文字、単語、行の出現頻度の計算機能を提供します。
+//! 詳しくは [`count`] 関数のドキュメントを見てください。
+
 use regex::Regex;
 use std::collections::HashMap;
 use std::io::BufRead;
 
+/// [`count`] で使うオプション
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum CountOption {
+    /// 文字ごとに頻度を数える
     Char,
+    /// 単語ごとに頻度を数える
     Word,
+    /// 行ごとに頻度を数える
     Line,
 }
 
+/// オプションのデフォルトは [`CountOption::Word`]
 impl Default for CountOption {
     fn default() -> Self {
         CountOption::Word
     }
 }
 
+/// input から 1 行ずつ UFT-8 文字列を読み込み、頻度を数える
+///
+/// 頻度を数える対象はオプションによって制御される
+/// * [`CountOption::Char`]: Unicode の 1 文字ごと
+/// * [`CountOption::Word`]: 正規表現 \w+ にマッチする単語ごと
+/// * [`CountOption::Line`]: \n または \r\n で区切られた 1 行ごと
+///
+/// # Panics
+///
+/// 入力が UTF-8 でフォーマットされていない場合にパニックする
 pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize> {
     let re = Regex::new(r"\w+").unwrap();
     let mut freqs = HashMap::new();
