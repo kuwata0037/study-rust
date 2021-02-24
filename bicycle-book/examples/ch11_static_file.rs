51daf1c58d1f9375c5d96bde2ac715bd5785896e
kuwata0037 <kuwata0037@gmail.com>
2021-02-24 22:46:12 +0900

    静的ファイルを返却する API サーバを実装
    実践Rust第11章

---
+++ bicycle-book/examples/ch11_static_file.rs
@@ -0,0 +1,8 @@
+use actix_web::{fs, server, App};
+
+fn main() {
+    server::new(|| App::new().handler("/", fs::StaticFiles::new(".").unwrap()))
+        .bind("localhost:3000")
+        .expect("Can not bind to port 3000")
+        .run();
+}
