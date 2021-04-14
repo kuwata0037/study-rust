384c2877f6dee1586e9332c5cf03096eeed57480
kuwata0037 <kuwata0037@gmail.com>
2021-04-14 22:02:08 +0900

    web-crawler の第3章を実装

--- web-crawler/src/main.rs
+++ web-crawler/src/main.rs
@@ -1,9 +1,27 @@
+use select::document::Document;
+use select::predicate::Name;
+use url::ParseError as UrlParseError;
+use url::Url;
+
 fn main() -> eyre::Result<()> {
     let response = reqwest::blocking::get("https://www.rust-lang.org")?;
-    let status = response.status();
+    let base_url = response.url().clone();
     let body = response.text()?;
-    println!("status = {:?}", status);
-    println!("body = {:?}", body);
+    let doc = Document::from(body.as_str());
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        match Url::parse(href) {
+            Ok(url) => {
+                println!("{}", url);
+            }
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}", url);
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
+    }
 
     Ok(())
 }
