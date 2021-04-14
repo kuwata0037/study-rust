017a92d8e9c6a79b3d1512a011027d7a4a79c69a
kuwata0037 <kuwata0037@gmail.com>
2021-04-14 22:47:23 +0900

    web-crawler の第4章の演習を実装

--- web-crawler/src/main.rs
+++ web-crawler/src/main.rs
@@ -3,7 +3,10 @@ use url::Url;
 use web_crawler::LinkExtractor;
 
 fn main() -> eyre::Result<()> {
-    let url = Url::parse("https://www.rust-lang.org")?;
+    let arg = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "https://www.rust-lang.org".to_string());
+    let url = Url::parse(arg.as_str())?;
     let client = ClientBuilder::new().build()?;
     let extractor = LinkExtractor::from_client(client);
 
