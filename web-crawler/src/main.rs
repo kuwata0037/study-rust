71b433f2332ab012c0b344afbeaaa5a1189824b2
kuwata0037 <kuwata0037@gmail.com>
2021-04-14 09:33:35 +0900

    web-crawler の第2章を実装

--- web-crawler/src/main.rs
+++ web-crawler/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eyre::Result<()> {
+    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
+    let status = response.status();
+    let body = response.text()?;
+    println!("status = {:?}", status);
+    println!("body = {:?}", body);
+
+    Ok(())
 }
