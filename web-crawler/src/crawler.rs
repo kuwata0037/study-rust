b92be94cb02ec498b188ce151f2bb77a284c1e6b
kuwata0037 <kuwata0037@gmail.com>
2021-04-16 21:34:53 +0900

    web-crawler の第7章を実装

---
+++ web-crawler/src/crawler.rs
@@ -0,0 +1,98 @@
+use std::borrow::Borrow;
+use std::collections::{HashSet, VecDeque};
+use std::hash::Hash;
+
+pub trait AdjacentNodes {
+    type Node;
+
+    fn adjacent_nodes(&self, v: &Self::Node) -> Vec<Self::Node>;
+}
+
+pub struct Crawler<'a, G: AdjacentNodes> {
+    graph: &'a G,
+    visit: VecDeque<G::Node>,
+    visited: HashSet<G::Node>,
+}
+
+impl<'a, G> Crawler<'a, G>
+where
+    G: AdjacentNodes,
+    G::Node: Clone + Hash + Eq + Borrow<G::Node>,
+{
+    pub fn new(graph: &'a G, start: G::Node) -> Self {
+        let mut visit = VecDeque::new();
+        let visited = HashSet::new();
+
+        visit.push_back(start);
+
+        Self {
+            graph,
+            visit,
+            visited,
+        }
+    }
+}
+
+impl<'a, G> Iterator for Crawler<'a, G>
+where
+    G: AdjacentNodes,
+    G::Node: Clone + Hash + Eq + Borrow<G::Node>,
+{
+    type Item = G::Node;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(v) = self.visit.pop_front() {
+            if self.visited.contains(&v) {
+                continue;
+            }
+
+            let adj = self.graph.adjacent_nodes(&v);
+            for u in adj.into_iter() {
+                if !self.visited.contains(&u) {
+                    self.visit.push_back(u);
+                }
+            }
+
+            self.visited.insert(v.clone());
+
+            return Some(v);
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct AdjVec(Vec<Vec<usize>>);
+
+    impl AdjacentNodes for AdjVec {
+        type Node = usize;
+
+        fn adjacent_nodes(&self, v: &Self::Node) -> Vec<Self::Node> {
+            self.0.get(*v).cloned().unwrap_or_default()
+        }
+    }
+
+    #[test]
+    fn test_bfs() {
+        let graph = AdjVec(vec![vec![1, 2], vec![0, 3], vec![3], vec![2, 0]]);
+
+        let crawler = Crawler::new(&graph, 0);
+        let nodes = crawler.collect::<Vec<usize>>();
+
+        assert_eq!(nodes, vec![0, 1, 2, 3]);
+    }
+
+    #[test]
+    fn test_bfs_exercise() {
+        let graph = AdjVec(vec![vec![1], vec![0, 2, 4], vec![0, 3], vec![0], vec![0]]);
+
+        let crawler = Crawler::new(&graph, 0);
+        let nodes = crawler.collect::<Vec<usize>>();
+
+        assert_eq!(nodes, vec![0, 1, 2, 4, 3]);
+    }
+}
