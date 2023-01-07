361aaf5af02929b1d0b103f58d93b30a6c125c93
kuwata0037 <kuwata0037@gmail.com>
2023-01-08 00:03:57 +0900

    Remove some modules

--- books/bicycle-book/src/bin/ch11_static_file.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::{fs, server, App};
-
-fn main() {
-    server::new(|| App::new().handler("/", fs::StaticFiles::new(".").unwrap()))
-        .bind("localhost:3000")
-        .expect("Can not bind to port 3000")
-        .run();
-}
