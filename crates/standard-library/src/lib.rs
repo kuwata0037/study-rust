d2d77a2b043a5c82ccd7441ae3cf89f095013a67
kuwata0037 <kuwata0037@gmail.com>
2023-03-20 10:38:36 +0900

    Study socket addr

--- crates/standard-library/src/lib.rs
+++ crates/standard-library/src/lib.rs
@@ -1,3 +1,4 @@
 mod declarative_macro;
+mod net;
 mod pattern;
 mod sync;
---
+++ crates/standard-library/src/net.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+    #[test]
+    fn test_socket_addr_v4() {
+        let socket = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+        assert!(socket.is_ipv4());
+        assert_eq!(socket.ip(), IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(socket.port(), 8080);
+    }
+
+    #[test]
+    fn test_socket_addr_v4_from_str() {
+        let socket: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+
+        assert!(socket.is_ipv4());
+        assert_eq!(socket.ip(), IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+        assert_eq!(socket.port(), 8080);
+    }
+}
