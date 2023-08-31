8ddbc9dbe679c5d60cafd6cf142dd80d294f5fd6
kuwata0037 <kuwata0037@gmail.com>
2023-08-31 23:08:49 +0900

    create user ハンドラーを追加

--- Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
+++ Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
@@ -1,12 +1,20 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
 
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
 
@@ -19,4 +27,24 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Debug, Serialize)]
+struct User {
+    id: u64,
+    name: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        name: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
 }
