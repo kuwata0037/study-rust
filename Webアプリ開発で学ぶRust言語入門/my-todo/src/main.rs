7ac92233ec34857b9f59fb63dcce0bf0b082dcf1
kuwata0037 <kuwata0037@gmail.com>
2023-08-31 22:49:11 +0900

    ログ出力を追加

--- Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
+++ Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
@@ -4,9 +4,13 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     let app = Router::new().route("/", get(root));
 
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
+
+    tracing::debug!("Listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
