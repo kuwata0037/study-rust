2861b777db16559bcbb8518c02f671bb0a96485f
kuwata0037 <kuwata0037@gmail.com>
2023-09-17 01:21:57 +0900

    graceful shutdownに対応

--- books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
+++ books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
@@ -38,6 +38,33 @@ async fn main() {
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
-        .unwrap();
+        .expect("fail start server");
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("signal received, staring graceful shutdown");
 }
