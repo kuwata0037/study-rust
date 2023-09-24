eab9a8b84b4019ac34b472065dd10b384afb43e6
kuwata0037 <kuwata0037@gmail.com>
2023-09-24 18:20:24 +0900

    Feature/web-rust (#83)
    * 存在しないURLパスを指定した場合のテストを追加
    
    * Repositoryを追加
    
    * repositoryとhandlerをモジュールに分割
    
    * TodoRepositoryForMemoryを実装
    
    * 一部修正
    
    * idの型をu32に変更
    
    * `/todos/:id`にハンドラーを登録
    
    * todosパスのハンドラーを実装
    
    * モジュール構成を改善
    
    * TodoRepositoryの返り値を改善
    
    * 微修正
    
    * APIテストを追加
    
    * リポジトリのasnyc対応
    
    * Postgresへ接続するリポジトリを実装
    
    * コメントなどを整理
    
    * DBに操作のシナリオテストを追加
    
    * labelを追加
    
    * graceful shutdownに対応
    
    * ラベルのメモリストアを実装

---
+++ books/Webアプリ開発で学ぶRust言語入門/README.md
@@ -0,0 +1,27 @@
+# Webアプリ開発で学ぶRust言語入門
+
+## 動作
+
+データベースを起動する。
+
+```sh
+make up
+```
+
+APIサーバーを起動する。
+
+```sh
+make dev
+```
+
+テストを実行する。
+
+```sh
+make test
+```
+
+データベースを終了する。
+
+```sh
+docker compose down
+```
--- books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
+++ books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
@@ -1,98 +1,70 @@
+//! # My todo API
+//!
+//! ## API
+//!
+//! - /todos
+//!     - GET: Todo情報の一覧取得
+//!     - POST: Todo情報の作成
+//! - /todos/:id
+//!     - GET: idに対応するTodo情報の取得
+//!     - PATCH: Todo情報の更新
+//!     - DELETE: Todo情報の削除
+
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
+use sqlx::PgPool;
+use web_rust_my_todo::{
+    handler::create_app,
+    repository::{label::LabelRepositoryForPostgres, todo::TodoRepositoryForPostgres},
 };
-use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = create_app();
+    tracing::debug!("connect to database");
+    let database_url = std::env::var("DATABASE_URL").expect("Undefined [DATABASE_URL]");
+    let pool = PgPool::connect(&database_url)
+        .await
+        .expect("fail connect database");
+
+    let todo_repository = TodoRepositoryForPostgres::new(pool.clone());
+    let label_repository = LabelRepositoryForPostgres::new(pool.clone());
+
+    let app = create_app(todo_repository, label_repository);
 
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
 
-    tracing::debug!("Listening on {}", addr);
+    tracing::debug!("listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
-        .unwrap();
+        .expect("fail start server");
 }
 
-fn create_app() -> Router {
-    Router::new()
-        .route("/", get(root))
-        .route("/users", post(create_user))
-}
-
-async fn root() -> &'static str {
-    "Hello, world!"
-}
-
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
-struct User {
-    id: u64,
-    name: String,
-}
-
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    let user = User {
-        id: 1337,
-        name: payload.username,
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
     };
 
-    (StatusCode::CREATED, Json(user))
-}
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
 
-#[cfg(test)]
-mod tests {
-    use axum::{body::Body, http::header, http::Method, http::Request};
-    use tower::ServiceExt;
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
 
-    use super::*;
-
-    #[tokio::test]
-    async fn should_return_hello_world() {
-        let req = Request::builder().uri("/").body(Body::empty()).unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK);
-
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-
-        assert_eq!(body, "Hello, world!");
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
 
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let req = Request::builder()
-            .uri("/users")
-            .method(Method::POST)
-            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-            .body(Body::from(r#"{ "username": "田中 太郎" }"#))
-            .unwrap();
-        let res = create_app().oneshot(req).await.unwrap();
-        assert_eq!(res.status(), StatusCode::CREATED);
-
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-        let user: User = serde_json::from_str(&body).expect("cannot convert User instance.");
-
-        assert_eq!(
-            user,
-            User {
-                id: 1337,
-                name: "田中 太郎".to_string()
-            }
-        );
-    }
+    tracing::info!("signal received, staring graceful shutdown");
 }
