91b3fc976051634b23de2e3b93758db63a43d699
kuwata0037 <kuwata0037@gmail.com>
2024-03-17 22:45:22 +0900

    refactor phone number tests

--- libraries/third-party/src/phone_number.rs
+++ libraries/third-party/src/phone_number.rs
@@ -4,13 +4,27 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("+1 (413) 487-0806")]
     #[case("+14134870806")]
-    #[case("+81 (3) 1234-5678")]
-    #[case("+81-3-1234-5678")]
+    #[case("+1-413-487-0806")]
+    #[case("+1 (413) 487-0806")]
+    fn test_parse_international_us_phone_number(#[case] phone_number: &str) {
+        let phone_number = phonenumber::parse(None, phone_number).unwrap();
+
+        assert_eq!(phone_number.country().id(), Some(Id::US));
+        assert_eq!(phone_number.national().to_string(), "4134870806");
+        assert_eq!(phone_number.to_string(), "+14134870806");
+    }
+
+    #[rstest]
     #[case("+81312345678")]
-    fn test_successful_parse_phone_number(#[case] phone_number: &str) {
-        phonenumber::parse(None, phone_number).unwrap();
+    #[case("+81-3-1234-5678")]
+    #[case("+81 (3) 1234-5678")]
+    fn test_parse_international_jp_phone_number(#[case] phone_number: &str) {
+        let phone_number = phonenumber::parse(None, phone_number).unwrap();
+
+        assert_eq!(phone_number.country().id(), Some(Id::JP));
+        assert_eq!(phone_number.national().to_string(), "312345678");
+        assert_eq!(phone_number.to_string(), "+81312345678");
     }
 
     #[rstest]
@@ -19,8 +33,12 @@ mod tests {
     #[case("03(1234)5678")]
     #[case("03 (1234) 5678")]
     #[case("+81312345678")]
-    fn test_successful_parse_japanese_phone_number(#[case] phone_number: &str) {
-        phonenumber::parse(Some(Id::JP), phone_number).unwrap();
+    fn test_parse_japanese_phone_number(#[case] phone_number: &str) {
+        let phone_number = phonenumber::parse(Some(Id::JP), phone_number).unwrap();
+
+        assert_eq!(phone_number.country().id(), Some(Id::JP));
+        assert_eq!(phone_number.national().to_string(), "312345678".to_string());
+        assert_eq!(phone_number.to_string(), "+81312345678");
     }
 
     #[rstest]
@@ -28,8 +46,9 @@ mod tests {
     #[case(Mode::International, "+81 3-1234-5678")]
     #[case(Mode::National, "03-1234-5678")]
     #[case(Mode::Rfc3966, "tel:+81-3-1234-5678")]
-    fn test_japanese_phone_number_format(#[case] mode: Mode, #[case] expected: &str) {
+    fn test_format_japanese_phone_number(#[case] mode: Mode, #[case] expected: &str) {
         let phone_number = phonenumber::parse(Some(Id::JP), "03(1234)5678").unwrap();
+
         assert_eq!(expected, phone_number.format().mode(mode).to_string());
     }
 }
