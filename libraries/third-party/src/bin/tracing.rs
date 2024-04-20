e33cb578e19aa5ead568e7704dc63b49fff0c234
kuwata0037 <kuwata0037@gmail.com>
2024-04-20 16:54:46 +0900

    tracing の動作確認実行ファイルを追加

---
+++ libraries/third-party/src/bin/tracing.rs
@@ -0,0 +1,88 @@
+use tracing_subscriber::layer::SubscriberExt;
+
+#[derive(Debug)]
+struct Parameter {
+    id: u32,
+    #[allow(dead_code)]
+    name: String,
+}
+
+#[tracing::instrument]
+fn service(param: Parameter) {
+    tracing::info!(?param, "call service");
+    use_case(param.id + 1);
+}
+
+#[tracing::instrument]
+fn use_case(id: u32) {
+    tracing::warn!("call use case");
+    infrastructure::repository((id + 1).to_string().as_str());
+}
+
+mod infrastructure {
+    #[tracing::instrument]
+    pub(super) fn repository(id: &str) {
+        tracing::error!("call repository");
+    }
+}
+
+fn main() {
+    tracing::subscriber::with_default(tracing_subscriber::FmtSubscriber::new(), || {
+        tracing::info!(format = "default", "start tracing subscriber");
+        service(Parameter {
+            id: 1,
+            name: "default".to_string(),
+        });
+        println!();
+    });
+
+    tracing::subscriber::with_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .compact()
+            .finish(),
+        || {
+            tracing::info!(format = "compact", "start tracing subscriber");
+            service(Parameter {
+                id: 10,
+                name: "compact".to_string(),
+            });
+            println!();
+        },
+    );
+
+    tracing::subscriber::with_default(
+        tracing_subscriber::fmt::SubscriberBuilder::default()
+            .pretty()
+            .finish(),
+        || {
+            tracing::info!(format = "pretty", "start tracing subscriber");
+            service(Parameter {
+                id: 100,
+                name: "pretty".to_string(),
+            });
+            println!();
+        },
+    );
+
+    tracing::subscriber::with_default(tracing_subscriber::fmt().json().finish(), || {
+        tracing::info!(format = "json", "start tracing subscriber");
+        service(Parameter {
+            id: 1000,
+            name: "json".to_string(),
+        });
+        println!();
+    });
+
+    tracing::subscriber::with_default(
+        tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer().json().flatten_event(true)),
+        || {
+            tracing::info!(format = "flatten json", "start tracing subscriber");
+            service(Parameter {
+                id: 2000,
+                name: "flatten json".to_string(),
+            });
+            println!();
+        },
+    );
+}
