2558fffaefde3efc5b3b0c936452576d3b5c9ec6
kuwata0037 <kuwata0037@gmail.com>
2024-11-28 23:39:19 +0900

    add create user

--- books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
+++ books/Webアプリ開発で学ぶRust言語入門/my-todo/src/main.rs
@@ -1,13 +1,29 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing, Router};
+use axum::{response::IntoResponse, routing, Json, Router};
+use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
+use tracing_subscriber::{filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", routing::get(root));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new()
+        .route("/", routing::get(root))
+        .route("/users", routing::post(create_user));
 
     let addr = SocketAddr::from((Ipv4Addr::LOCALHOST, 3000));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    tracing::info!("listening on {addr}");
+
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
@@ -15,4 +31,24 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+#[derive(Debug, Serialize)]
+struct User {
+    id: u64,
+    user_name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct CreateUser {
+    user_name: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        user_name: payload.user_name,
+    };
+
+    (StatusCode::CREATED, Json(user))
 }
